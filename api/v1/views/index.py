a941c7c09263f16c97fe3a2969d54c64202bb8de
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-28 11:36:57 -0700

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """ import app_views """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -9,3 +10,17 @@ def api_status():
     """ return in JSON mode {status: OK} """
     response = {"status": "OK"}
     return jsonify(response)
+
+
+@app_views.route('/stats')
+def stats():
+    """ count the object by classes """
+    count = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(count)
