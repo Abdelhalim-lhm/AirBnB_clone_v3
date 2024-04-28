bc267886781bc489c62d1ce730fb1cd03e803620
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-04-28 11:52:02 -0700

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ import modules """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,13 @@ app.register_blueprint(app_views)
 def teardown_app(exception):
     """ teardown app def """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(exception):
+    """ handle error 404 """
+    response = {"error": "Not found"}
+    return jsonify(response)
 
 
 if __name__ == "__main__":
