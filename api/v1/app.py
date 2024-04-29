176b1c4742fde6b032cb4645ddee8c597bab0044
fadwa <fadwa.lacham@gmail.com>
2024-04-28 18:59:42 -0700

    12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
